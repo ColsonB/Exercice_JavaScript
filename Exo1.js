b399c9b7d82b12dc8079b4bbb6b79a8c4c85e8d1
Baptiste Colson <bcolson@la-providence.net>
2021-01-26 16:18:40 +0100

    Commit

--- Exo1.js
+++ Exo1.js
@@ -2,9 +2,16 @@ function calculepérimétre(){
 
     var longueur = parseInt(prompt("Longueur"));
     var largeur = parseInt(prompt("Largeur"));
-    var résultat = longueur+largeur;
+    var résultat = longueur+largeur*2;
     alert(résultat);
 
+}
 
+function calculair(){
+
+    var longueur = parseInt(prompt("Longueur"));
+    var largeur = parseInt(prompt("Largeur"));
+    var résultat = longueur * largeur;
+    alert(résultat);
 
 }
---
+++ Exo2.js
@@ -0,0 +1,15 @@
+function calculpérimétre(){
+
+    var rayon = parseInt(prompt("Rayon"));
+    var résultat = 2*3.14*rayon;
+    alert(résultat);
+
+}
+
+function calculair(){
+
+    var rayon = parseInt(prompt("Rayon"));
+    var résultat = 3.14*rayon*2;
+    alert(résultat);
+
+}
