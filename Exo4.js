ed5e3701dd16f8520937ff9f4c56807805d9e25c
Baptiste Colson <bcolson@la-providence.net>
2021-01-28 11:15:21 +0100

    Commit

--- Exo4.js
+++ Exo4.js
@@ -1,14 +1,10 @@
 var tab = [-2, 1, 2];
 
-function additionne(){
-
-    var x;
-    var addition;
-    addition = x*2;
-
-}
+const additionne = (x) => x*2;
 
 function affiche(){
 
+    alert(additionne(tab[0]));
+    alert(additionne(tab[tab.length-1]));
 
 }
