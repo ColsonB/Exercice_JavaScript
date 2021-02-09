6bce0f3568eb9e62afe953d3bbb4e2728dccf045
Baptiste Colson <bcolson@la-providence.net>
2021-02-09 17:04:29 +0100

    Commit

---
+++ Exo11.js
@@ -0,0 +1,8 @@
+function modif_paragraphe(){
+    const text = document.getElementById('text')
+    document.getElementById('text') = innerHTML='<i>Corrigé</i>';
+}
+function centrage_h1(){
+    const titre = document.getElementById('titre')
+    document.getElementById('titre') = this.textAlign='center';
+}
