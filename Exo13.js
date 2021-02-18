771b7fc1181e33574f123c8696b1370c0c322ac4
Baptiste Colson <bcolson@la-providence.net>
2021-02-18 12:19:39 +0100

    Commit

---
+++ Exo13.js
@@ -0,0 +1,6 @@
+function affiche(){
+    alert(navigator.userAgent);
+    alert(navigator.appName);
+    alert(navigator.appVersion);
+    alert(navigator.geolocation);
+}
---
+++ Exo14.js
@@ -0,0 +1,17 @@
+function init(){
+    var nom = document.getElementById(name1);
+    var prenom = document.getElementById(prenom1);
+    var info = document.getElementById(info1);
+    alert(nom, prenom, info);
+}
+
+function fctSubmit(){
+    ObjectBoutonSubmit.onclick=fctSubmit;
+}
+
+function remise_a_zero(){
+    alert("Le Formulaire va être réinitialiser");
+} 
+
+
+
